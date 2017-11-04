254152767df5682f5a4a665f9cae7e1797e290b6
Paul Smith <paul@numruff.com>
2017-11-03 23:02:57 -0700

    Initial development

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name 'nginx'
 maintainer 'Paul Smith'
 maintainer_email 'paul@numruff.com'
 license 'Apache-2.0'
-description 'Installs/Configures nginx'
-long_description 'Installs/Configures nginx'
+description 'INstalls/configures a simple web site using the Nginx web server.'
+long_description IO.read("#{File.dirname(__FILE__)}/README.md")
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
